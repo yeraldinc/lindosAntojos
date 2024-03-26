acb4743f63a5b9eac01df2c4123d41b597e44d52
Yerald <yeraldinemmanuel@gmail.com>
2024-03-25 23:47:27 -0500

    creando el menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <h1>hola mujer</h1> 
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>HOLA MUNDO</h1>
     </div>
   );
 }
